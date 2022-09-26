0c436f98763dc715e985e7d3eb4178c15377f005
David Campa <dcampa89@gmail.com>
2022-09-26 16:48:59 -0700

    service worker and push

--- push.js
+++ push.js
@@ -1,5 +1,12 @@
 var push = require('web-push');
 
-let vapid = push.generateVAPIDKeys();
+let vapidKeys = {
+    publicKey: 'BHjVcZrWH55IoFt6QOyS41FgSuib-a1niIJ_KJcY7McB6KbSbQoPYc6I4SNDpkFhA6CcYLBfvNcKd82Baw-l-qY',
+    privateKey: '2JMwxlVKfzfiIEDJDplJ_qxQh-eXjDKt9rw3AE1p5tY'
+  }
 
-console.log(vapid);
+push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey);
+
+let sub ={};
+
+push.sendNotification(sub, 'test message');
