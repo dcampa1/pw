b27d52e64652c7bb8e3a4e9db12f6e3fb2b32553
David Campa <dcampa89@gmail.com>
2022-09-26 17:03:29 -0700

    added sub endpoint

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {
+  "endpoint": "https://fcm.googleapis.com/fcm/send/erVLAY4BGbY:APA91bGju8o9aqd3VTmcVkyWxxq5golLpjWBw5PD6lQzyDgVNyAZ27W5rYzuLP6oR_3BRkOYeZr4nAqveWWEhd8NvwW_Ujfjw4vewOsVGJpzX_Ib4abHtnvuD8Q5nmaf1jikEzzkwiVF",
+  "expirationTime": null,
+  "keys": {
+    "p256dh": "BBYxBiei04oBtVIgoUX1nOxHEayhzFrpn0NbXNsQN_z1f87DaxI8DYc62e41vGpis4FEzfQjyDhmAeF0WNXBsP4",
+    "auth": "oJz8AJ2nGjx9hK2ytJNJ4Q"
+  }
+};
 
 push.sendNotification(sub, 'test message');
